f2ddd36ae330c0ea157dcefe79f3e89c7651db5d
CesarDRRDev <raizio.dev@icloud.com>
2026-01-30 16:40:59 -0600

    Added Mentum.ico resource for launcher branding

--- Mentum v1.2.4.bat
+++ Mentum v1.2.4.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB1GRQ6NAE+1EbsQ5+n//NakrUMLUfArOJvT3/SbMOwc60Hhcdgk1XU6
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdFy5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZkkaGErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlbMaAs=
+::ZQ05rAF9IAHYFVzEqQIdLRVAWQnCEGqvCLYU7fqb
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmHMn+vC4pNprq1u9Wro1gTV+o6arD63rmHLq4W8kCE
+::fBEirQZwNQPfEVWB+kM9LVsJDCmHMn+vC5kd/eb45++Vwg==
+::cRolqwZ3JBvQF1fEqQIHIRVQQxORfGa7D7sI7eb64emC4kAcUOd/fp3S37qKLPlz
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATEVotweksGGkSvPVmzCPUu6eHh5uXn
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmi5ks1Jh9YVUS1NWW+CaIPqOX65uSTp0McWuE6OJvU1bju
+::Zh4grVQjdCyDJGyX8VAjFB1GRQ6NAE+1EbsQ5+n//Naqp0MJQe8AKcGJlOCxDOEG5EbscIQS935XkcVCCQNdHg==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 chcp 65001 >nul
 setlocal
